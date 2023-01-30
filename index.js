065f89b8b9b3fa18479fd10cbc1ddeac178b1be7
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-01-30 00:09:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json())
+
+app.get('/sum/:n1/:n2', (req, res) => {
+  const { n1, n2 } = req.params;
+  const result = parseInt(n1) + parseInt(n2)
+  res.json({
+    "Resultado": result
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
