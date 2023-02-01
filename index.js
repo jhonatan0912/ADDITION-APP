8319c53357762437f1ad61688e2a590dcca6f5e0
Jhonatan <68581196+jhonatan0912@users.noreply.github.com>
2023-02-01 15:43:58 +0000

    add endpoints

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/',(req,res)=>{
+  res.json({
+    "endpoints": "/sum/{number1}/{number2}"
+  })
+})
+
 app.get('/sum/:n1/:n2', (req, res) => {
   const { n1, n2 } = req.params;
   const result = parseInt(n1) + parseInt(n2)
